a004901290cc50312f38d86546d030db8b83f236
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-05-30 14:51:07 +0200

    Initial clean Cypress project setup

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'pvx3v3',
+  allowCypressEnv: false,
+
+  e2e: {
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
+    viewportWidth: 800,
+    viewportHeight: 600,
+    chromeWebSecurity: false,
+    testIsolation: false,
+    watchForFileChanges: false
+  },
+});
